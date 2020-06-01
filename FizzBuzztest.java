0eeb41dd98ab516b57693e47baf8d695279d811a
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-06-01 09:33:40 -0400

    created a successful test returning a string with a number

--- FizzBuzztest.java
+++ FizzBuzztest.java
@@ -1,5 +1,19 @@
 package FizzBuzz;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzztest {
 
+    @Test
+    public void test() {
+
+    }
+
+    @Test
+    public void shouldprocessInput() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.processNumber(1));
+    }
 }
